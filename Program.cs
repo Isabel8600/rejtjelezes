68501a222c74a1bf8256227d4b67d0c28761ab60
Izabella Együd <138508152+Isabel8600@users.noreply.github.com>
2023-07-04 11:21:41 +0200

    The file is stored.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
 
 namespace rejtjelezes
 {
@@ -6,7 +10,40 @@ namespace rejtjelezes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's get this started!");
+            Console.WriteLine("Let's get started!");
+            
+            #region beolvasás
+            try
+            {
+                List<string> list = new List<string>();
+                StreamReader sr = new StreamReader("words.txt", Encoding.Default); //azért nincs elérési út, mert a bin-->Debug-->netcoreapp3.1 mappába bemásoltam a beolvasandó dokumentumot
+                while (sr.EndOfStream == false)
+                {
+                    string s = sr.ReadLine();
+                    if (s.Trim().Length ==0)
+                    {
+                        continue;
+                    }
+                    list.Add(s);
+                    //Console.WriteLine(s); //teszt
+                }
+            }
+            catch 
+            {
+                throw;
+            }
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Done. The file is stored.");
+            #endregion
+          
+
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
